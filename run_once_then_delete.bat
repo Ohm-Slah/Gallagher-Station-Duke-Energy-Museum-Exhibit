2be92048fefa35beee772435aae6553bfd19e092
Elliot Eickholtz <72700028+Ohm-Slah@users.noreply.github.com>
2021-10-30 12:35:10 -0400

    Delete run_once_then_delete.bat

--- run_once_then_delete.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-git clone https://github.com/turtlebrosforever/Gallagher-Station-Duke-Energy-Museum-Exhibit.git
-
-echo "If successful, please delete this file, it is no longer necessary."
-pause
