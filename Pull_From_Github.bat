665fd2f2c1f495143acf7aa583a235cb5118a8a8
Elliot Eickholtz <72700028+Ohm-Slah@users.noreply.github.com>
2021-10-30 12:40:58 -0400

    Update Pull_From_Github.bat

--- Pull_From_Github.bat
+++ Pull_From_Github.bat
@@ -3,3 +3,5 @@
 git pull https://github.com/turtlebrosforever/Gallagher-Station-Duke-Energy-Museum-Exhibit.git
 
 echo "Success! Please make sure to run the Push To Github file when finished. Elliot won't review it until a pull-request is made in Github."
+
+pause
