416c96e2543ca447520cd311c311807e70a00c03
Elliot Eickholtz <72700028+Ohm-Slah@users.noreply.github.com>
2021-11-17 16:36:18 -0500

    Add files via upload

---
+++ main/main.ino
@@ -0,0 +1,61 @@
+/*
+ * File name:         "main.ino"
+ * Contributor(s):    Elliot Eickholtz
+ * Last edit:         11/17/21
+ * Code usage:
+ * This is the main Arduino file used in the creation of a museum exhibit of the Gallagher Station is New Albany, Indiana. 
+ * The intended usage of this code is to simulate the responsibilities of a control station operator of a coal-fired power plant, 
+ * while keeping the content and interactions approachable for a person with little-to-no experience in the field. 
+ * This code is sequential, and can be subdivided into 5 main sections represented numerically from 'zero' to 'four'.
+ */
+
+#include "phases.h"
+
+bool zeroPassed = false;
+bool onePassed = false;
+bool twoPassed = false;
+bool threePassed = false;
+bool fourPassed = false;
+
+void setup() 
+{
+  initialization();
+  
+}
+
+void loop() 
+{
+  zeroPassed = phaseZero();
+  
+  if (phaseZero())
+  {
+    onePassed = phaseOne();
+    if (phaseOne())
+    {
+      twoPassed = phaseOne();
+      if (phaseTwo())
+      {
+        threePassed = phaseThree();
+        if (phaseThree())
+        {
+          fourPassed = phaseFour();
+          if (phaseFour())
+          {
+            completion();
+          } else {
+            failure();
+          }
+        } else {
+          failure();
+        }
+      } else {
+        failure();
+      }
+    } else {
+      failure();
+    }
+  } else {
+    failure();
+  }
+  
+}
