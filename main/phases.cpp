155265342d4c3c97d462ae57c04faed71c690d4c
Elliot Eickholtz <ellioteickholtz@gmail.com>
2021-12-06 16:15:07 -0500

    Cleaned up and commented more

--- main/phases.cpp
+++ main/phases.cpp
@@ -1,51 +1,39 @@
 /*
    File name:         "phases.cpp"
    Contributor(s):    Elliot Eickholtz, Matthew Wrocklage, Jackson Couch, Andrew Boehm
-   Last edit:         12/4/21
+   Last edit:         12/6/21
    Code usage:
    This is a file containing all functions used in each of the five phases of the "main.ino" file.
-
 */
 
 #include "phases.h"
 
-// Create a variable to store the servo position:
+// Create a variable to store the servo position
 int angle = 0;
+
+// reintantiate global use variables
 volatile bool phaseChange = false;
 volatile byte currentPhase = 0;
 
-Encoder AirandVoltage(2, 3);
-Encoder CoalandSteam(18, 19);       /* Creates an Encoder object, using 2 pins. Creates mulitple Encoder objects, where each uses its own 2 pins. The first pin should be capable of interrupts.
-                               If both pins have interrupt capability, both will be used for best performance.
-                               Encoder will also work in low performance polling mode if neither pin has interrupts.
-*/
+// Creates an Encoder object, using 2 pins.
+Encoder AirandVoltage(ENCODER1APIN, ENCODER1BPIN); 
+Encoder CoalandSteam(ENCODER2APIN, ENCODER2BPIN); 
 
-TM1637 tm(4, 5);            /* Library instantiation for 7-segment display
-                               Pin 4 -> DIO
-                               Pin 5 -> CLK
-*/
+// Library instantiation for 7-segment display
+TM1637 tm(SEGCLK, SEGDIO);  
 
+// This is for the audio
+TMRpcm tmrpcm; 
+                               
+// intantiate variables for led blinking interrupts
 const uint16_t halfTwoSec = 31250;
 const uint16_t fullFourSec = 62500;
 volatile byte ledState = 0;
 volatile long ledCount = 0;
 
-//integers for the stepper motor
-const int dirPin = 30;
-const int stepPin = 31;
-const int enPin = 32;
-const int homePin = 33;
+// variables for the stepper motor
 const int stepsPerRevolution = 200;
-
 long stepperPosition;
-
-TMRpcm tmrpcm; //This is for the audio
-
-bool zeroPassed = false;
-bool onePassed = false;
-bool twoPassed = false;
-bool threePassed = false;
-bool fourPassed = false;
 
 void initialization()
 {
@@ -53,62 +41,60 @@ void initialization()
      This fuction is run once on startup.
      This is to simply initialize everything needed.
   */
-  pinMode(16, INPUT_PULLUP); //Phone Switch
-  pinMode(15, OUTPUT); //Light bulb
-  pinMode(30, INPUT); //Confirm button
+  pinMode(PHONESWITCHPIN, INPUT_PULLUP); //Phone Switch
+  pinMode(LIGHTBULBSWITCHPIN, OUTPUT); //Light bulb
   pinMode(31, INPUT); //Send Power button
-  pinMode(22, OUTPUT); //Phase 1 Red LED
-  pinMode(23, OUTPUT); //Phase 1 Green LED
-  pinMode(24, OUTPUT); //Phase 2 Red LED
-  pinMode(25, OUTPUT); //Phase 2 Green LED
-  pinMode(26, OUTPUT); //Phase 3 Red LED
-  pinMode(27, OUTPUT); //Phase 3 Green LED
-  pinMode(28, OUTPUT); //Phase 4 Red LED
-  pinMode(29, OUTPUT); //Phase 4 Green LED
+  pinMode(P1RLED, OUTPUT); //Phase 1 Red LED
+  pinMode(P1GLED, OUTPUT); //Phase 1 Green LED
+  pinMode(P2RLED, OUTPUT); //Phase 2 Red LED
+  pinMode(P2GLED, OUTPUT); //Phase 2 Green LED
+  pinMode(P3RLED, OUTPUT); //Phase 3 Red LED
+  pinMode(P3GLED, OUTPUT); //Phase 3 Green LED
+  pinMode(P4RLED, OUTPUT); //Phase 4 Red LED
+  pinMode(P4GLED, OUTPUT); //Phase 4 Green LED
   pinMode(LED_ON_BOARD, OUTPUT); //LED pin of Arduino Mega
   pinMode(MOTOR_PIN, OUTPUT); //DC Motor Pin
-  pinMode(servoPin, OUTPUT); //Servo motor Pin
-  pinMode(20, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(20), resetPhases, FALLING);
-  pinMode(21, INPUT_PULLUP);
+  pinMode(SERVOPIN, OUTPUT); //Servo motor Pin
+  pinMode(RESETSWITCHPIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(RESETSWITCHPIN), resetPhases, FALLING);
+  pinMode(CONFIRMBUTTONPIN, INPUT_PULLUP);
 
-  initSevenSegment();
+  
 
   TCCR1A = 0;
   TIMSK1 = (1 << OCIE1A);
   sei();
   Serial.begin(9600);
-
   delay(100);
 
   StepperSetup();
+  initSevenSegment();
+  tmrpcm.speakerPin = SDCSPIN;
+
+  if (!SD.begin(SDCSPIN))
+    error();
 
   if (!serialResponse("RESPOND")) error();
-  //Audio
-  tmrpcm.speakerPin = 46;          //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
-  //Complimentary Output or Dual Speakers:
-  //pinMode(10,OUTPUT); Pin pairs: 9,10 Mega: 5-2,6-7,11-12,46-45
-  Serial.begin(9600);
-  if (!SD.begin(SD_ChipSelectPin))
-  {
-    Serial.println("SD fail");
-    return;
-  }
+
+  
 
 }
 
 void reset()
 {
+  /*
+   * This function can be ran when all items on board need to be reset
+   */
   homeStepper();
 
   AirandVoltage.write(0);
   CoalandSteam.write(0);
   ledBlink(0, 1000);
-  digitalWrite(23, HIGH);
-  digitalWrite(25, HIGH);
-  digitalWrite(27, HIGH);
-  digitalWrite(29, HIGH);
-  digitalWrite(15, LOW);
+  digitalWrite(P1GLED, HIGH);
+  digitalWrite(P2GLED, HIGH);
+  digitalWrite(P3GLED, HIGH);
+  digitalWrite(P4GLED, HIGH);
+  digitalWrite(LIGHTBULBSWITCHPIN, LOW);
   servoMove(1);
   setDCMotor(0);
   tm.clearScreen();
@@ -117,9 +103,10 @@ void reset()
 byte phaseZero()
 {
   /*
-     This fuction is the 'awaiting user input' phase. This will run for a maximum of three hours,
+     This fuction is the 'awaiting user input' phase. This will run for a maximum of X hours,
      and will then enter a sleep state. The only difference in the sleep state is what the raspberry pi displays.
-     When exiting the sleep state, homing everthing like the initialization stage is needed.
+     When exiting the sleep state, reseting function is needed.
+     This currently does nothing.
   */
 
   if (!serialResponse("PHASE ZERO")) error();
@@ -132,14 +119,13 @@ byte phaseOne()
 {
   /*
      This function is the first phase of the display.
-
      Steps:
      play intro vid
      play phase 1 instruction vid
      balance air and coal encoders until temp servo gauge is at specified position,
      simultaneously waiting for confirm button to be pressed
      if result on servo gauge is within eror margins, continue onto next phase.
-     if result is outside error margins, play fail video? three tries?
+     if result is outside error margins, play fail video
   */
   int prevPos = 0;
   //tmrpcm.loop(1);
@@ -169,7 +155,7 @@ byte phaseOne()
 
   ledBlink(0B00000001, 1000);
 
-  while (digitalRead(21))
+  while (digitalRead(CONFIRMBUTTONPIN))
   {
     if (phaseChange) return 1;
     coalRead = encoderRead('C');
@@ -207,9 +193,9 @@ byte phaseOne()
         airAngle = 0;
     }
 
+    //apply law of sines as well as right triangle maths to solve for temp
     airLine = ((float)bottomLine * sin((float)coalAngle * PI / 180)) / sin(((float)180 - coalAngle - airAngle) * PI / 180);
     tempLine = sin((float)airAngle * PI / 180) * airLine;
-    //Serial.print(map((int)tempLine, 0, 1374, 255, 0));Serial.print(" : ");Serial.print(airAngle);Serial.print(" : ");Serial.println(coalAngle);
     servoMove(map((int)tempLine, 0, 1374, 255, 0));
     delay(5);
   }
@@ -217,7 +203,7 @@ byte phaseOne()
   if (abs(tempLine - 926) < 50 && abs(airAngle - coalAngle) < 5)
   {
     //Serial.println("SUCCESS");
-    digitalWrite(23, LOW);
+    digitalWrite(P1GLED, LOW);
     servoMove(79);
     return 2;
   } else {
@@ -240,7 +226,7 @@ byte phaseTwo()
   int16_t steam = 0;
   ledBlink(0B00000101, 1000);
 
-  while (digitalRead(21))
+  while (digitalRead(CONFIRMBUTTONPIN))
   {
     if (phaseChange) return 1;
 
@@ -265,7 +251,7 @@ byte phaseTwo()
     steamRead = encoderRead('C');
   }
 
-  digitalWrite(25, LOW);
+  digitalWrite(P2GLED, LOW);
   if (phaseChange) return 1;
   return 3;
 }
@@ -284,7 +270,7 @@ byte phaseThree()
   int16_t steam = 0;
   int16_t steamPrev = 0;
 
-  while (digitalRead(21))
+  while (digitalRead(CONFIRMBUTTONPIN))
   {
     if (phaseChange) return 1;
     steamRead = encoderRead('C');
@@ -294,7 +280,7 @@ byte phaseThree()
       CoalandSteam.write(0);
       if (steam > steamPrev)
       {
-        digitalWrite(dirPin, LOW);
+        digitalWrite(DIRPIN, LOW);
         for (int i = steamPrev; i < steam; i++)
         {
           stepperTick();
@@ -303,7 +289,7 @@ byte phaseThree()
         steamPrev = steam;
       } else if (steam < steamPrev)
       {
-        digitalWrite(dirPin, HIGH);
+        digitalWrite(DIRPIN, HIGH);
         for (int i = steamPrev; i > steam; i--)
         {
           stepperTick();
@@ -314,10 +300,10 @@ byte phaseThree()
     }
 
   }
-  digitalWrite(27, LOW);
-  digitalWrite(29, LOW);
-  digitalWrite(enPin, HIGH);
-  digitalWrite(15, HIGH);
+  digitalWrite(P3GLED, LOW);
+  digitalWrite(P4GLED, LOW);
+  digitalWrite(ENPIN, HIGH);
+  digitalWrite(LIGHTBULBSWITCHPIN, HIGH);
   return 10; //temporarily skip phase four
 }
 
@@ -325,10 +311,11 @@ byte phaseFour()
 {
   /*
      This function is the fourth phase of the display.
+     This phase currently does nothing.
   */
   if (!serialResponse("PHASE FOUR")) error();
 
-  while (digitalRead(21))
+  while (digitalRead(CONFIRMBUTTONPIN))
   {
     if (phaseChange) return 1;
   }
@@ -338,7 +325,7 @@ byte phaseFour()
 bool serialResponse(char com[])
 {
   /*
-     This function takes a predefined string command and confirms a serial response from the raspberry pi running processing.
+     This function takes a predefined string command and confirms a serial response from a computer running processing sketch.
      Predefined commands: "RESPOND" "RING" "PHASE ZERO" "PHASE ONE" "PHASE TWO" "PHASE THREE" "PHASE FOUR" "FAILURE" "COMPLETE"
   */
   uint8_t attempts = 0;
@@ -373,14 +360,14 @@ void failure()
   */
   delay(1000);
   if (!serialResponse("RING")) error();
-  while(!digitalRead(16)) {};
+  while (!digitalRead(PHONESWITCHPIN)) {};
   if (!serialResponse("FAILURE")) error();
   //tmrpcm.disable();
   fail_state_audio();
   delay(5000);
   tmrpcm.disable();
-  
-  while(digitalRead(16)) {};
+
+  while (digitalRead(PHONESWITCHPIN)) {};
   return true;
 }
 
@@ -390,7 +377,7 @@ void completion()
      This function is the completion state of the display.
   */
   if (!serialResponse("COMPLETE")) error();
-  delay(5000);
+  delay(7500);
   return true;
 }
 
@@ -402,9 +389,9 @@ void error()
   Serial.println("CRITICAL ERROR");
   while (1)
   {
-    digitalWrite(13, HIGH);
+    digitalWrite(LED_ON_BOARD, HIGH);
     delay(200);
-    digitalWrite(13, LOW);
+    digitalWrite(LED_ON_BOARD, LOW);
     delay(200);
   }
 }
@@ -423,7 +410,7 @@ void servoMove(uint16_t position)
   /*
      This function recieves a position value and moves the servo to that position. 0-255
   */
-  analogWrite(servoPin, position);
+  analogWrite(SERVOPIN, position);
 }
 
 int8_t encoderRead(char enc)
@@ -483,19 +470,19 @@ void ledStateChange(byte State)
   /*
      This function takes in a byte (pins 22-29) representing the wanted state of the LEDs.
   */
-//  for (int i = 22; i < 30; i++)
-//  {
-//    digitalWrite(i, ~(ledState & (0x01 << (i - 22) ) ) );
-//  }
-   digitalWrite(22, !(State&0b00000001));
-   //digitalWrite(23, !(State&0b00000010));
-   digitalWrite(24, !(State&0b00000100));
-   //digitalWrite(25, !(State&0b00001000));
-   digitalWrite(26, !(State&0b00010000));
-   //digitalWrite(27, !(State&0b00100000));
-   digitalWrite(28, !(State&0b01000000));
-   //digitalWrite(29, !(State&0b10000000));
-   //Serial.println(State);
+  //  for (int i = 22; i < 30; i++)
+  //  {
+  //    digitalWrite(i, ~(ledState & (0x01 << (i - 22) ) ) );
+  //  }
+  digitalWrite(P1RLED, !(State & 0b00000001));
+  //digitalWrite(23, !(State&0b00000010));
+  digitalWrite(P2RLED, !(State & 0b00000100));
+  //digitalWrite(25, !(State&0b00001000));
+  digitalWrite(P3RLED, !(State & 0b00010000));
+  //digitalWrite(27, !(State&0b00100000));
+  digitalWrite(P4RLED, !(State & 0b01000000));
+  //digitalWrite(29, !(State&0b10000000));
+  //Serial.println(State);
 }
 
 void ledBlink(byte LED, int Time)
@@ -541,22 +528,22 @@ void ledBlink(byte LED, int Time)
 void StepperSetup()
 {
   // Declare pins as Outputs
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(enPin, OUTPUT);
-  pinMode(homePin, INPUT);
+  pinMode(STEPPIN, OUTPUT);
+  pinMode(DIRPIN, OUTPUT);
+  pinMode(ENPIN, OUTPUT);
+  pinMode(HOMEPIN, INPUT);
 
   // Set motor direction clockwise
-  digitalWrite(dirPin, HIGH);
+  digitalWrite(DIRPIN, HIGH);
   // disable stepper
-  digitalWrite(enPin, HIGH);
+  digitalWrite(ENPIN, HIGH);
 }
 
 void homeStepper()
 {
-  digitalWrite(enPin, LOW);
+  digitalWrite(ENPIN, LOW);
 
-  while (!digitalRead(homePin))
+  while (!digitalRead(HOMEPIN))
   {
     stepperTick();
     delay(4);
@@ -566,9 +553,9 @@ void homeStepper()
 
 void stepperTick()
 {
-  digitalWrite(stepPin, HIGH);
+  digitalWrite(STEPPIN, HIGH);
   delayMicroseconds(1);
-  digitalWrite(stepPin, LOW);
+  digitalWrite(STEPPIN, LOW);
   stepperPosition = (stepperPosition % stepsPerRevolution) + 1;
 }
 
--- main/phases.h
+++ main/phases.h
@@ -1,7 +1,7 @@
 /*
  * File name:         "phases.h"
  * Contributor(s):    Elliot Eickholtz, Matthew Wrocklage, Jackson Couch, Andrew Boehm
- * Last edit:         12/4/21
+ * Last edit:         12/6/21
  * Code usage:
  * This is an instantiation file for "phases.cpp".
  * Any libraries used or function declarations are located here.
@@ -11,24 +11,55 @@
 #ifndef SETUP_H
 #define SETUP_H
 
-//PINS 11 & 12 CANNOT BE USED FOR PWM
-
-#define servoPin 8 // Define the servo pin:
-
-#define LED_ON_BOARD 13
+//define all of the pins used with particular names for identification //
+#define ENCODER1APIN 2
+#define ENCODER1BPIN 3
+#define SEGCLK 4
+#define SEGDIO 5
+#define SERVOPIN 8
 #define MOTOR_PIN 10
-#define SD_ChipSelectPin 53   // example uses hardware SS pin 53 on Mega2560 or 10 for the UNO 
+//PINS 11 & 12 CANNOT BE USED FOR PWM
+#define LED_ON_BOARD 13
+#define LIGHTBULBSWITCHPIN 15
+#define PHONESWITCHPIN 16
+#define ENCODER2APIN 18
+#define ENCODER2BPIN 19
+#define RESETSWITCHPIN 20
+#define CONFIRMBUTTONPIN 21
+#define P1RLED 22
+#define P1GLED 23
+#define P2RLED 24
+#define P2GLED 25
+#define P3RLED 26
+#define P3GLED 27
+#define P4RLED 28
+#define P4GLED 29
+#define DIRPIN 30
+#define STEPPIN 31
+#define ENPIN 32
+#define HOMEPIN 33
+#define AUDIOPIN 46
+//50, 51, & 52 are used by the SD card reader
+#define SDCSPIN 53
+////////////////////////////////////////////////////////////////////////
 
+
+//include all libraries used //
 #include <Arduino.h>
 #include <Encoder.h>  //https://github.com/PaulStoffregen/Encoder
 #include <TM1637.h>   //https://github.com/AKJ7/TM1637
 #include <SD.h>
 #include <TMRpcm.h>   //https://www.arduino.cc/reference/en/libraries/tmrpcm/
 #include <SPI.h>
+///////////////////////////////
 
+
+// instantiate important global variables //
 extern volatile byte currentPhase;
 extern volatile bool phaseChange;
+////////////////////////////////////////////
 
+// instantiate all functions used in program //
 void initialization();
 byte phaseZero();
 byte phaseOne();
@@ -56,6 +87,5 @@ void displayDigitalNumber(float value);
 void setDCMotor(uint16_t pwmValue);
 void fail_state_audio();
 int mapValues(int x, int in_min, int in_max, int out_min, int out_max);
-
-void test();
+////////////////////////////////////////////////
 #endif
