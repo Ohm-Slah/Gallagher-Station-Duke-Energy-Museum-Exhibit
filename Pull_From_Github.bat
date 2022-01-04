9cd765a5a1cb0d149d69141aac9c31a2a3fe9418
Elliot Eickholtz <72700028+Ohm-Slah@users.noreply.github.com>
2022-01-04 08:24:01 -0500

    Delete Pull_From_Github.bat

--- Pull_From_Github.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-git pull https://github.com/Ohm-Slah/Gallagher-Station-Duke-Energy-Museum-Exhibit.git
-
-echo "Success! Please make sure to run the Push To Github file when finished COMPLETELY. Elliot won't review it until a pull-request is made in Github."
-
-pause
